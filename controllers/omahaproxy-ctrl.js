285d3dd874882503918283450dfc00296ec7a351
Guus Goossens <guus.goossens@gmail.com>
2013-08-05 14:58:37 +0200

    Show random image.

--- controllers/omahaproxy-ctrl.js
+++ controllers/omahaproxy-ctrl.js
@@ -1,6 +1,7 @@
 angular.module('omahaproxyApp.controllers')
 	.controller('OmahaproxyCtrl', function( $scope, $q, OmahaproxyService, GoogleImagesService ) {
 
+		// Get a list of chromebooks from the OmahaProxy and add an image from every device.
 		$scope.chromebooks = OmahaproxyService.get().then( addImagesToChromebooks );
 
 
@@ -14,14 +15,18 @@ angular.module('omahaproxyApp.controllers')
 
 		// Helper function to make the .map() more readable.
 		function addImage( chromebook ) {
+			// Fire of a search request and handle the returning data.
 			return GoogleImagesService.search( chromebook.name ).then(function( searchRes ) {
-				var imageRes = searchRes.data.responseData.results[0]
+				// Just take a random image, make a clone, modify it and return the new enriched chromebook object.
+				var result = searchRes.data.responseData.results
+				  , random = Math.floor( Math.random() * result.length )
+				  , image = result[ random ]
 				  , chromebookClone = angular.copy( chromebook )
 
 				chromebookClone.image = {
-					src: imageRes.tbUrl,
-					width: imageRes.tbWidth,
-					height: imageRes.tbHeight
+					src: image.tbUrl,
+					width: image.tbWidth,
+					height: image.tbHeight
 				}
 
 				return chromebookClone;
